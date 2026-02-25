6fa4ad79b7f3adcb3ece2cb90c9da60c1d80e373
Enzo-1022 <enzofernandes560@gmail.com>
2026-02-25 16:47:14 -0300

    Pequena Correção no Middleware que valida as sessões, corrigida a validação do cabeçalho authorization antes se o cabeçalho fosse undefined acontecia um erro na aplicação.

--- middlewares/middlewareSessao.js
+++ middlewares/middlewareSessao.js
@@ -6,43 +6,43 @@ import Sessoes from '../models/mSessoes.js'; // Modelo de sessôes
 
 export default async function middlewareSessao(req, res, next) {
   try {
-    var token = req.headers.authorization.split(' ')[1];
+
+    var token = !req.headers.authorization? undefined : req.headers.authorization.split(' ')[1]; // Adicionei a validação para caso não exista nada no cabeçalho aurhorization, fazer mais validações 25/02/2026.
 
     if (token == null || token == undefined) 
     {
       return res.status(401).json({Erro : `Não Autorizado! Token Indefinido`});
     }
-    else
+
+    const VerificacaoToken = jwt.verify(token, process.env.PasswordSession)
+
+    const Sessao = await Sessoes.findAll({
+      where : {
+        Id_Usuario : VerificacaoToken.Id_Usuario
+      } 
+    });
+
+    // Validar se no método jwt.verify ja tem a verificação de expiração por time/hora
+
+    if (!Sessao.length) 
     {
-      const VerificacaoToken = jwt.verify(token, process.env.PasswordSession)
-
-      const Sessao = await Sessoes.findAll({
-        where : {
-          Id_Usuario : VerificacaoToken.Id_Usuario
-        } 
-      });
-
-      // Validar se no método jwt.verify ja tem a verificação de expiração por time/hora
-
-      if (!Sessao.length) 
-      {
-        return res.status(401).json({Erro : "Não autorizado! Token Invalido!"});
-      }
-
-      // add o user id a requisição para caso algum middleware subsequente precise
-      req.userID = VerificacaoToken.Id_Usuario;
-
-      next();
+      return res.status(401).json({Erro : "Não autorizado! Token Invalido!"});
     }
+
+    // add o user id a requisição para caso algum middleware subsequente precise
+    req.userID = VerificacaoToken.Id_Usuario;
+
+    next();
+
   } catch (error) {
+
     if (error.name == 'TokenExpiredError' && error.name == 'JsonWebTokenError') 
     {
       return res.status(401).json({ Erro: "Não Autorizado! Sessão Expirada." });
     }
-    else
-    {
-      return res.status(500).json({ Erro: "Erro Interno do Servidor!"+error })
-    }
+
+    return res.status(500).json({ Erro: `${error}` });
+
   }
 }
 
