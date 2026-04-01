774a35e6e1357d28601ea9f19326fe92ed84956b
Enzo-1022 <enzofernandes560@gmail.com>
2026-04-01 15:39:47 -0300

    Criação da Lógica do Endpoint que Retorna o Catalogo com Os Produtos, Criada a Lógica de Paginação para que não haja uma query muito longa no banco de dados

--- controllers/controllerProdutos.js
+++ controllers/controllerProdutos.js
@@ -1,15 +1,50 @@
+import { body, validationResult } from "express-validator";
 import mProdutos from "../models/mProdutos.js";
 
-export async function catalogo(req, res) {
-    try {
-        const Produtos = await mProdutos.findAll();
+export const Catalogo = [ // Falta Validar
+    body('Pagina').trim().escape().toInt(),
 
-        return res.status(200).json({Produtos: Produtos.toJSON()});
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-    } catch (error) {
-        return res.status(500).json({Erro: error})
+            if (!errors.isEmpty()) {
+                return res.status(400).json({Erro : await errors[0]})
+            }
+
+            const QtdProdutos = await mProdutos.count();
+
+            var qtdPaginas;
+            
+            if (QtdProdutos >= 15) // Calculo de paginas com base nos produtos cadastrados no banco
+            {
+                if (QtdProdutos % 15){
+                    qtdPaginas = Math.ceil(QtdProdutos / 15);
+                }
+                else {
+                    qtdPaginas = QtdProdutos / 15;
+                }
+            }
+            else {
+                qtdPaginas = 1
+            }
+
+
+            if(!req.params.Pagina){
+                const Produtos = await mProdutos.findAll({limit : 15});
+
+                return res.status(200).json({Produtos: await Produtos.toJSON(), QtdPaginas : qtdPaginas});
+            }
+
+            const Produtos = await mProdutos.findAll({limit : 15, offset : req.params.Pagina * 15});
+
+            return res.status(200).json({Produtos: await Produtos.toJSON(), QtdPaginas : qtdPaginas});
+
+        } catch (error) {
+            return res.status(500).json({Erro: error})
+        }
     }
-}
+];
 
 export async function produto(req, res){
     try {
